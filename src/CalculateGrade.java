53ba4376b05966598df18169fa8b4c8e94b6773c
9Pattarapon <pptrpw123@gmail.com>
2021-09-01 22:05:27 +0700

    PoomEditGrade

--- src/CalculateGrade.java
+++ src/CalculateGrade.java
@@ -13,19 +13,19 @@ public class CalculateGrade {
 	public String calculate() { 
 		String grade; 
 	
-	if(score >= 80) {
-		grade = "A"; 
-	}else if(score >= 75) { 
-		grade = "B"; 
-	}else if(score >= 70) { 
-		grade = "B+"; 
-	}else if(score >= 60) { 
-		grade = "C"; 
-	}else if(score >= 50) { 
-		grade = "D";
-	}else{ 
-		grade = "F"; } 
-	
-	return grade; 
+		if(score >= 80) {
+			grade = "A"; 
+		}else if(score >= 75) { 
+			grade = "B"; 
+		}else if(score >= 70) { 
+			grade = "B+"; 
+		}else if(score >= 60) { 
+			grade = "C"; 
+		}else if(score >= 50) { 
+			grade = "D";
+		}else{ 
+			grade = "F"; } 
+		
+		return grade; 
 	} 
 }
